fcd590a001834f11400ba26443beafd02b52d82b
vlad <vlad@leonis.mrc.alcatel.ro>
2015-10-24 15:06:47 +0300

    demo for drawing pictures

--- elm/Shape.elm
+++ elm/Shape.elm
@@ -25,9 +25,10 @@ area shape =
     Ellipse    r1 r2   -> pi*r1*r2
     Polygon    (v1::vs)->  let polyArea vs =
                                  case vs of
-                                   (v2::v3::vs') -> triAreapic4 v1 v2 v3 + polyArea (v3::vs')
+                                   (v2::v3::vs') -> triArea v1 v2 v3 + polyArea (v3::vs')
                                    otherwise     -> 0
                            in polyArea vs
+
 
 triArea : Vertex -> Vertex -> Vertex -> Float
 triArea v1 v2 v3 =
