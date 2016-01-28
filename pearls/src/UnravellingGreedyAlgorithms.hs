e1d754b248635a79efd3c5f2f4138a65ae6b61bf
ibnHatab <vlad@leonis.mrc.alcatel.ro>
2016-01-28 17:52:45 +0200

    greedy recap with unravel

--- pearls/src/UnravellingGreedyAlgorithms.hs
+++ pearls/src/UnravellingGreedyAlgorithms.hs
@@ -1,5 +1,7 @@
 module UnravellingGreedyAlgorithms where
 
+import Data.List
+
 type Data = [Datum]
 type Datum = String
 
@@ -64,8 +66,43 @@ cross (f,g)(x,y) = (f x, g y)
 
 -- shortest upravel
 
+minBy f = foldl1 (cmp f)
+cmp f u v = if f u <= f v then u else v
 
 supravel :: Ord a => [a] -> [[a]]
 supravel = minBy length . filter (all up) . unravels
 
-unravels
+unravels :: Ord a => [a] -> [[[a]]]
+-- concatMap :: Foldable t => (a -> [b]) -> t a -> [b]
+unravels = foldr (concatMap . prefixes) [[]]
+prefixes :: t -> [[t]] -> [[[t]]]
+prefixes x [] = [[[x]]]
+prefixes x (xs:xss) =  ((x:xs) : xss) : map (xs :) (prefixes x xss)
+
+up [] = True
+up [_] = True
+up (x : y : xs) = x <= y && up xs
+
+-----------------
+
+upravel :: Ord a => [a] -> [[[a]]]
+-- upravel = filter (all up) . unravels
+-- easy fission law
+upravel = foldr (concatMap . uprefixes) [[]]
+uprefixes x [] = [[[x]]]
+uprefixes x (xs:xss) = if x <= head xs
+                       then ((x:xs):xss) : map (xs:) (uprefixes x xss)
+                       else (uprefixes x xss)
+
+-- minBy length . uprefixes x ~~> insert x
+heads :: Ord a => [[a]] -> [a]
+heads = sort . map head
+
+insert' :: Ord a => a -> [[a]] -> [[a]]
+insert' x [] = [[x]]
+insert' x (xs:xss) = if x <= head xs
+                     then (x:xs) : xss
+                     else xs : insert' x xss
+
+supravel' :: Ord a => [a] -> [[a]]
+supravel' = foldr insert' []
