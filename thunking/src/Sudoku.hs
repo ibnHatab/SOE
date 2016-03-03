5c036489561db2e6902ae544ed37e4cb09d118ce
ibnHatab <vlad@leonis.mrc.alcatel.ro>
2016-03-03 12:12:22 +0200

    before prune

--- thunking/src/Sudoku.hs
+++ thunking/src/Sudoku.hs
@@ -12,6 +12,8 @@ digits = ['1'..'9']
 blank :: Digit -> Bool
 blank = (== '0')
 
+----------------------------------
+
 solve :: Grid -> [Grid]
 solve = filter valid . completions
 
@@ -19,38 +21,41 @@ valid :: Grid -> Bool
 valid g = all nodups (rows g) &&
           all nodups (cols g) &&
           all nodups (boxs g)
-
-nodups :: Eq a => [a] -> Bool
-nodups [] = True
-nodups (x:xs) = x `notElem` xs &&
-                nodups xs
-
-rows = id
-
-cols [xs] = [[x] | x <- xs]
-cols (xs:xss) = zipWith (:) xs (cols xss)
-
-boxs = map ungroup . ungroup . map cols . group . map group
-  where ungroup = concat
-        group [] = []
-        group xs = take 3 xs : group (drop 3 xs)
+  where
+    nodups :: Eq a => [a] -> Bool
+    nodups [] = True
+    nodups (x:xs) = x `notElem` xs && nodups xs
+    rows = id
+    cols [xs] = [[x] | x <- xs]
+    cols (xs:xss) = zipWith (:) xs (cols xss)
+    boxs = map ungroup . ungroup . map cols . group . map group
+      where ungroup = concat
+            group [] = []
+            group xs = take 3 xs : group (drop 3 xs)
 
 completions :: Grid -> [Grid]
-completions = expand . choices
+completions = expand . prune . choices
 
 choices :: Grid -> Matrix [Digit]
 choices = map (map choice)
-
-choice :: Digit -> [Digit]
-choice d = if blank d then digits else [d]
+  where
+    choice :: Digit -> [Digit]
+    choice d = if blank d then digits else [d]
 
 expand :: Matrix [Digit] -> [Grid]
 expand = cp . map cp
+  where
+    cp :: [[a]] -> [[a]]
+    cp [] = [[]]
+    cp (xs:xss) = [x:ys | x <- xs, ys <- yss]
+               where yss = cp xss
 
-cp :: [[a]] -> [[a]]
-cp [] = [[]]
-cp (xs:xss) = [x:ys | x <- xs, ys <- yss]
-              where yss = cp xss
+prune :: Matrix [Digit] -> Matrix [Digit]
+prune = undefined
+  where pruneRow row = map (remove fixed) row
+          where fixed = [d | [d] <- row]
+                remove ds [x] = [x]
+                remove ds xs = filter (`notElem` ds) xs
 
 {--
 4..!96.!37.
